5f163d7e467b45b0b3349ec6e981904b181a244a
Chabarys <alissonchabaribery6@gmail.com>
2020-03-13 21:39:29 -0300

    JavaScript • adicionando plugins de animacao

---
+++ Bikraft/web/javascript/script.js
@@ -0,0 +1,16 @@
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote",
+        time: 5000
+    })
+    
+    new SimpleSlide({
+        slide: "portifolio",
+        nav: true,
+        time: 5000
+    })
+}
+
+if (window.SimpleAnime) {
+    new SimpleAnime()
+}
